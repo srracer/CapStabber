759ab098d8103dbac79b935e058dfcc49c43f030
DESKTOP-03IQ21T\slide <slideruleracer@gmail.com>
2017-06-05 00:25:55 -0400

    Initial commit attempt

---
+++ Debug/images.d
@@ -0,0 +1,11 @@
+# FIXED
+
+images.obj: ../images.c
+images.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h
+images.obj: C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h
+images.obj: C:/ti/TivaWare_C_Series-2.1.3.156/grlib/grlib.h
+
+../images.c: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdint.h: 
+C:/ti/ccsv7/tools/compiler/ti-cgt-arm_16.9.0.LTS/include/stdbool.h: 
+C:/ti/TivaWare_C_Series-2.1.3.156/grlib/grlib.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../tm4c123gh6pm.cmd 
+
+LIB_SRCS += \
+../driverlib.lib \
+C:/ti/TivaWare_C_Series-2.1.3.156/grlib/ccs/Debug/grlib.lib 
+
+C_SRCS += \
+../Kentec320x240x16_ssd2119_spi.c \
+../frame.c \
+../images.c \
+../jmrlogo.c \
+../main.c \
+../tm4c123gh6pm_startup_ccs.c \
+../touch.c 
+
+C_DEPS += \
+./Kentec320x240x16_ssd2119_spi.d \
+./frame.d \
+./images.d \
+./jmrlogo.d \
+./main.d \
+./tm4c123gh6pm_startup_ccs.d \
+./touch.d 
+
+OBJS += \
+./Kentec320x240x16_ssd2119_spi.obj \
+./frame.obj \
+./images.obj \
+./jmrlogo.obj \
+./main.obj \
+./tm4c123gh6pm_startup_ccs.obj \
+./touch.obj 
+
+OBJS__QUOTED += \
+"Kentec320x240x16_ssd2119_spi.obj" \
+"frame.obj" \
+"images.obj" \
+"jmrlogo.obj" \
+"main.obj" \
+"tm4c123gh6pm_startup_ccs.obj" \
+"touch.obj" 
+
+C_DEPS__QUOTED += \
+"Kentec320x240x16_ssd2119_spi.d" \
+"frame.d" \
+"images.d" \
+"jmrlogo.d" \
+"main.d" \
+"tm4c123gh6pm_startup_ccs.d" \
+"touch.d" 
+
+C_SRCS__QUOTED += \
+"../Kentec320x240x16_ssd2119_spi.c" \
+"../frame.c" \
+"../images.c" \
+"../jmrlogo.c" \
+"../main.c" \
+"../tm4c123gh6pm_startup_ccs.c" \
+"../touch.c" 
+
+
